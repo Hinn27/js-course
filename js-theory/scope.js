3648fbea2c11ce7655127c5a13d4e3f107782efa
Hinn <duc107243@donga.edu.vn>
2026-07-29 17:15:43 +0700

    hiểu về scope của biến trong JS

---
+++ js-theory/scope.js
@@ -0,0 +1,42 @@
+// scope quyết định khả năng truy cập của biến
+// global scope: ở đâu cũng được
+const globalVar = "đâu cũng được";
+function global(globalVar) {
+  console.log(globalVar);
+}
+
+// function scope: truy cập trong hàm
+function funcVar() {
+  var funcVar = "khởi tạo ở đâu dùng ở đó";
+  console.log(funcVar);
+}
+console.log(funcVar); // nhốt k ra được
+
+// block scope: let và const bị nhốt trong nhà tù hình { } (như if, for, while)
+{
+  let blockLet = "tôi bị nhốt";
+  const blockConst = "thả tao raaaaaa";
+  var blockVar = "lêu lêu đồ bị nhốt =))";
+
+  console.log(blockLet);
+  console.log(blockConst);
+  console.log(blockVar);
+}
+
+console.log(blockLet);
+console.log(blockConst);
+console.log(blockVar);
+
+// lexical scope: định nghĩa toàn cục thì khi được gọi sẽ lấy giá tri toàn cục chứ không phải nơi hàm được gọi
+const x = "toàn cục"; // định nghĩa global
+
+function printX() {
+  console.log(x); // lấy gtri global
+}
+
+function changeX() {
+  const x = "cục bộ"; // gán lại x
+  console.log(x); // vẫn nhận gtr đầu (toàn cục)
+}
+
+changeX();
